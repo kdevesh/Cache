a83ca9b540b1ad2cdc35286ae4e2728023f05c50
d0k026s <devesh.kumar@walmart.com>
2021-06-06 13:49:11 +0530

    "Added DoublyLinkedList Algo"

---
+++ src/test/java/algorithms/DoublyLinkedListImplTest.java
@@ -0,0 +1,47 @@
+package algorithms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DoublyLinkedListImplTest {
+
+  DoublyLinkedListImpl<Integer> dll = new DoublyLinkedListImpl<>();
+
+  @Test
+  void addNodeAtLast() {
+    dll.addNodeAtLast(new DoublyLinkedListNode<>(1));
+  }
+
+  @Test
+  void isItemPresent() {
+    assertFalse(dll.isItemPresent());
+  }
+
+  @Test
+  void getFirstNode() {
+    assertNull(dll.getFirstNode());
+  }
+
+  @Test
+  void getLastNode() {
+    assertNull(dll.getLastNode());
+  }
+
+  @Test
+  void addElementAtLast() {
+    DoublyLinkedListNode<Integer> node = dll.addElementAtLast(2);
+    assertNotNull(node);
+    assertEquals(2,dll.getLastNode().data);
+    assertEquals(2,dll.getFirstNode().data);
+  }
+
+  @Test
+  void removeNode() {
+    DoublyLinkedListNode<Integer> node = dll.addElementAtLast(1);
+    dll.removeNode(node);
+    assertFalse(dll.isItemPresent());
+    assertNull(dll.getFirstNode());
+    assertNull(dll.getLastNode());
+  }
+}
